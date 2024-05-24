8c71461feed1cde8a80277cf89b0720d37f47ba4
Sahoon Hussain Ansari <162339117+Sahoon1@users.noreply.github.com>
2024-05-24 20:24:41 +0500

    work

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+
+// $num  = 10;
+// $num_02 = "10";
+
+// airthmrtic operator
+// echo($num + $num_02);
+// echo($num - $num_02);
+// echo($num * $num_02);
+// echo($num / $num_02);
+// echo($num % $num_02);
+
+//assignment operator
+
+// $num =-5 ;
+// $num =+59 ;
+// echo($num);
+
+// comparison operator
+
+// echo ($num != $num_02);
+// echo ($num === $num_02);
+// echo ($num >= $num_02);
+//  echo ($num !== $num_02);
+
+// $admin = "sahoon";
+// $pass = 12345;
+// $admin_02 = "haris";
+// $pass_02 = 123456;
+
+// if ($admin == "sahoon" && $pass == 12345 || $admin_02 == "haris" && $pass_02 == 123456) {
+//     echo ("Welcome Admin");
+// } else {
+//     echo ("Login Filed");
+// }
+
